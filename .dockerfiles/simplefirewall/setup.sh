74ebe4397190546b2ec9973ad2bd3d721e9ee3f6
darioguarra <dario.guarracino@wellnet.it>
2021-04-16 18:15:10 +0200

    add ICMP files

---
+++ .dockerfiles/simplefirewall/setup.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# eth0 is the public network interface
+# eth1 is the local network interface 
+
+echo "Setting up your amazing firewall..."
+
+echo 1 > /proc/sys/net/ipv4/ip_forward
+# forward requests
+
+iptables -F OUTPUT
+
+# Allow outcoming ICMP traffic
+iptables \
+    --append OUTPUT \
+    --proto icmp \
+    --jump ACCEPT
+    
+# Accept incoming ICMP traffic
+iptables \
+    --append INPUT \
+    --proto icmp \
+    --jump ACCEPT 
+
+
+exec "$@"
